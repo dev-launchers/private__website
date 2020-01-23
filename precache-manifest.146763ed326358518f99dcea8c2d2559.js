7b4ebb0bbe16d78454d09fddab08613b5cc1de2a
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-01-23 12:49:14 +0000

    Deploy dev-launchers/website to github.com/dev-launchers/website.git:master

---
+++ precache-manifest.146763ed326358518f99dcea8c2d2559.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b84367a68548f53221cfd61ffbc7a88",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4e82b075f1ac2c3b0421",
+    "url": "/static/css/2.5dbdccff.chunk.css"
+  },
+  {
+    "revision": "6baea87461f1890c1b8d",
+    "url": "/static/css/main.3042b3cd.chunk.css"
+  },
+  {
+    "revision": "4e82b075f1ac2c3b0421",
+    "url": "/static/js/2.203c70ff.chunk.js"
+  },
+  {
+    "revision": "9f28864a408e3024fc92f0d728f704e6",
+    "url": "/static/js/2.203c70ff.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6baea87461f1890c1b8d",
+    "url": "/static/js/main.69c9d94a.chunk.js"
+  },
+  {
+    "revision": "41a5d57d22fe2be65567",
+    "url": "/static/js/runtime-main.9c1a700e.js"
+  },
+  {
+    "revision": "9c884c33e5dc1a679a65ba8b333bc20d",
+    "url": "/static/media/hero-render.9c884c33.png"
+  },
+  {
+    "revision": "63c1d8f95dec8eb45563bdcb79725211",
+    "url": "/static/media/logo-full.63c1d8f9.png"
+  },
+  {
+    "revision": "ec624a5873705c3fd7cf70fb2da6f5a4",
+    "url": "/static/media/logo-monogram.ec624a58.png"
+  },
+  {
+    "revision": "eacc94aa6f5aa0c00f9de0d74bf70b1c",
+    "url": "/static/media/spark-symbol.eacc94aa.png"
+  }
+]);
