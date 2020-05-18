1b8cefd2bdddfb52e8caf7c04ebcc80a6bbf62bf
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-05-18 00:35:49 +0000

    Deploy dev-launchers/private__website to github.com/dev-launchers/private__website.git:master

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.478b4949d4285048a02dac9f9954c3b7.js"
+  "/precache-manifest.eb9b5a31a6f87629d85a1a724010d218.js"
 );
 
 self.addEventListener('message', (event) => {
